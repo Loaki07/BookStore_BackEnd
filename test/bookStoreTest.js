f2238b6372ddc33204f99479457f157ae02efe10
sendo07 <loakesh.i@gmail.com>
2020-12-02 17:04:30 +0530

    wip: Test cases for api

---
+++ test/bookStoreTest.js
@@ -0,0 +1,15 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+// Assertion Style
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Login API', () => {
+  /**
+   * @describe Testing LogIn POST request
+   */
+  describe("POST /login")
+
+})
